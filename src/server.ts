ffca23043fb8f512e63bc4fe8eec555d886272b8
duke <dukedaniel.tolentino@gmail.com>
2026-09-14 19:15:14 +0800

    try

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
 const PORT = 5000;
+
+app.use(cors());
+app
 
 app.get("/api/test", (req, res) => {
   res.json({
@@ -10,6 +14,6 @@ app.get("/api/test", (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
